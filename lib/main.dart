b6e9f953fdf9f07871f8edaf4cc26349d0989e6c
angel <angelicaizabella90@gmail.com>
2025-05-07 16:36:52 +0700

    fasum with gemini ai

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:shimmer/main.dart';
 import 'firebase_options.dart';
+import 'package:fasilitas_umum/screens/splash_screen.dart';
+
 //import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fasum',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: SplashScreen(),
     );
   }
 }
